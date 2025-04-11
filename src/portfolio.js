769556ceb7c2dab965810d3f50ddb747f0490612
MihaellK <79888949+MihaellK@users.noreply.github.com>
2025-04-11 14:55:23 -0300

    add links

--- src/portfolio.js
+++ src/portfolio.js
@@ -10,7 +10,8 @@ const about = {
   role: 'Software Developer',
   description:
     "I am a developer working with Web, Augmented Reality, and Virtual Reality, specializing in game development.", 
-  resume: 'https://docs.google.com/document/d/e/2PACX-1vTz-FZaBAfRKwzs2aUi4aESAeYgtiMs1239IKl75_CFl_c7Bu5W1nYq4Wr40f5kTKsllsAgpgbMmKGy/pub',
+  resume: 'https://docs.google.com/document/d/e/2PACX-1vSh14oKDyt4gELqtWPPg7RYrI1ROd5Dreng5teoDdLcLAcM3iqfMkSWHUmo6UbQgiw3QYXGZBNiRwDa/pub',
+  itchio: 'https://mihaellk.itch.io',
   social: {
     linkedin: 'https://www.linkedin.com/in/mihaellalves/',
     github: 'https://github.com/MihaellK',
@@ -20,14 +21,14 @@ const about = {
 const gameProjects = [
   {
     name: 'Kard Race',
-    briefDescription:'In-Development Racing Card Game. Selected for Trovalad Incubation Program',
+    briefDescription:'In-Development Racing Card Game. Selected for Trovalab Incubation Program',
     description:
       'Low-Poly Racing Card Game conceived during the HUG Jam, it was soon selected for incubation at Trovadoria. Since then, it has been showcased and featured in various game events, including EAI Jogos, HUG Verso, FIAP, and Inteli GameLab Showcase. I worked as a Game Designer, Lead Programmer, and contributed to the business aspects. It is currently my graduation thesis (TCC)',
     image: 'https://raw.githubusercontent.com/MihaellK/cleanfolio/refs/heads/main/public/imgs/capa_pitchDeck.jpeg',
     images: ['https://github.com/MihaellK/cleanfolio/blob/main/public/imgs/print-game-fia-version.jpeg?raw=true', 'https://github.com/MihaellK/cleanfolio/blob/main/public/imgs/trovalab-pitchday.JPG?raw=true',],
     stack: ['Unity', 'C#', 'Game Design', 'Design Patterns', 'AI Navmesh', 'Business',],
     responsibilities: ['Designed the core gameplay loop integrating cards and racing mechanics.', 'Architected and implemented the Race AI system using Unity’s NavMesh.', 'Successfully formatted and submitted the project for a public call.',],
-    achievements: [ 'EAI Jogos – Showcased to the public at ', 'HUGVerso Pitch Competition – Selected project', 'Trovalab – Selected for Trovadoria’s Incubation Program','Pitch Deck - Created and presented a professional pitch deck',],
+    achievements: [ 'Trovalab – Selected for Trovadoria’s Incubation Program', 'EAI Jogos – Showcased to the public', 'HUGVerso Pitch Competition – Selected project','Pitch Deck - Created and presented a professional pitch deck',],
     sourceCode: '',
     livePreview: '',
   },
@@ -36,11 +37,11 @@ const gameProjects = [
     briefDescription:'Escape Room VR developed as part of the Immersive Technologies module in the Computer Engineering course at Inteli',
     description:
       'Escape Room VR developed as part of the Immersive Technologies module in the Computer Engineering course at Inteli. Created in just three weeks, this was my first experience with Virtual Reality, where I learned more about dynamic lighting and optimization techniques. I worked as a Game Designer and Programmer.',
-    image: 'https://picsum.photos/id/227/200/300',
-    images: ['https://picsum.photos/id/238/200/300', 'https://picsum.photos/id/225/200/300',],
+    image: 'https://github.com/MihaellK/cleanfolio/blob/main/public/imgs/sunnyrise-vr-pitch-cover.jpeg?raw=true',
+    images: ['https://github.com/MihaellK/cleanfolio/blob/main/public/imgs/sunnyrise-vr-menu.jpeg?raw=true', 'https://github.com/MihaellK/cleanfolio/blob/main/public/imgs/sunnyrise-vr-bedroom.jpeg?raw=true',],
     stack: ['Virtual Reality', 'Unity', 'C#', 'Dynamic Lights', 'Enviromnent Design','Game Design'],
     responsibilities: ['Optimized game performance with Light Probes, Texture Resizing, and Occlusion Culling, ensuring smoother gameplay and enhanced visual quality on VR hardware.', 'Programmed puzzle logic and mechanics.', 'Integrated Meta Quest 3 with Unity and structured Git repository for seamless VR development and team collaboration.',],
-    achievements: [ 'High-Quality Prototype – A first contact with VR in a 3-week project.',],
+    achievements: [ 'High-Quality Prototype – First contact with VR in a 3-week project.',],
     sourceCode: '',
     livePreview: '',
   },
@@ -93,6 +94,7 @@ const skills = [
   'C#',
   'Python',
   'JavaScript',
+  'AI',
   'HTML',
   'CSS',
   'TypeScript',
@@ -102,6 +104,8 @@ const skills = [
   'React',
   'NextJS',
   'Unity',
+  'Virtual Reality',
+  'Augmented Reality',
   'Godot',
   'Game Maker Studio',
   'Git',
